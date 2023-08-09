a432802a05da9f99d185bcf5ef454bd561fa4641
Tiru <thiru53@gmail.com>
2023-08-09 20:15:03 +0530

    Task-1 commit

--- src/main/java/com/MangementApplication/entity/Product.java
+++ src/main/java/com/MangementApplication/entity/Product.java
@@ -3,10 +3,18 @@ package com.MangementApplication.entity;
 
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 
 import java.time.LocalDate;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name="product")
 public class Product {
--- src/main/java/com/MangementApplication/entity/ProductRequestDTO.java
+++ src/main/java/com/MangementApplication/entity/ProductRequestDTO.java
@@ -1,4 +1,9 @@
 package com.MangementApplication.entity;
 
+import lombok.Data;
+
+@Data
 public class ProductRequestDTO {
+    private String name;
+    private double price;
 }
